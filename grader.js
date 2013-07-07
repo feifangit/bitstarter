8d3f8992c1cb10c6c1b3ccf77e4cbcf8fd97a81e
FAN FEI <feifan.pub@gmail.com>
2013-07-07 21:58:21 +0000

    support check tags from an URL

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require("restler")
+
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -35,6 +37,7 @@ var assertFileExists = function(infile) {
     }
     return instr;
 };
+
 
 var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
@@ -61,14 +64,33 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var parse = function(htmlFile, checkFile){
+    var checkJson = checkHtmlFile(htmlFile, checkFile);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'URL link', null, null)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+    if (program.url){
+        rest.get(program.url).on('complete', function(result) {
+            if (result instanceof Error) {
+                console.error("Access URL %s error. Exiting.", program.url);
+                process.exit(1);
+            }else{
+                fs.writeFileSync("urldata", result); // write to tmp file urldata
+                parse("urldata", program.checks);
+                fs.unlinkSync("urldata");
+            }
+        });
+    }else{
+        parse(program.file, program.checks);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
