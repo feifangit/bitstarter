dff5e774ce161731b048539edeb38818001eff89
FEI FAN <feifan.pub@gmail.com>
2013-07-15 23:36:00 -0700

    add boostrap as CSS framework

--- web.js
+++ web.js
@@ -3,6 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+app.use(express.static(__dirname + '/static'));
 
 app.get('/', function(request, response) {
   //response.send('Hello World 2!');
@@ -10,7 +11,7 @@ app.get('/', function(request, response) {
   response.send(content.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 5000; //todo 8080
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
